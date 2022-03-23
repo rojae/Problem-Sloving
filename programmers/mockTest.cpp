ee6dc64af4e2bae931f465d93d6f801248029621
rojae <rojae@kakao.com>
2022-03-23 23:54:55 +0900

    Feat : 문제풀이 추가

---
+++ programmers/mockTest.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers);
+
+int student1[] = {1, 2, 3, 4, 5};
+int student2[] = {2, 1, 2, 3, 2, 4, 2, 5};
+int student3[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+int cnt[3] = {0,};
+
+int main(){
+    vector<int> answers = {1,2,3,4,5};
+    vector<int> ans = solution(answers);
+
+    for(int i=0; i<ans.size(); i++)
+        cout << ans[i] << ' ';
+
+    return 0;
+}
+
+vector<int> solution(vector<int> answers) {
+    vector<int> ans;
+    int m = 0;
+
+    for(int i=0; i<answers.size(); i++){
+        if(student1[i % 5] == answers.at(i))
+            cnt[0]++;
+        if(student2[i % 8] == answers.at(i))
+            cnt[1]++;
+        if(student3[i % 10] == answers.at(i))
+            cnt[2]++;
+    }
+
+    m = max(cnt[0], max(cnt[1], cnt[2]));
+
+    for(int i=0; i<3; i++){
+        if(cnt[i] == m)
+            ans.push_back(i+1);
+    }
+
+    return ans;
+}
