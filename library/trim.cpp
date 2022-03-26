e48f415d2d9c7361b7278b9524cf920f100fad98
rojae <rojae@kakao.com>
2022-03-27 01:26:04 +0900

    Feat : 라이브러리 추가

---
+++ library/trim.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+    
+using std::string;
+
+// trim from left 
+inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+	s.erase(0, s.find_first_not_of(t));
+	return s;
+}
+// trim from right 
+inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+	s.erase(s.find_last_not_of(t) + 1);
+	return s;
+}
+// trim from left & right 
+inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+	return ltrim(rtrim(s, t), t);
+}
+
+int main()
+{
+    string str = "             가나다라마바사   ";
+	std::cout << str << std::endl;
+	trim(str);
+	std::cout << str << std::endl;
+}
